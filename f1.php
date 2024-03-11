22604b9e240bf35f0afb725ce42fe78bd90f804a
MaryamHmayed <mariam.hmayed.88@gmail.com>
2024-03-12 00:07:36 +0200

    first function

---
+++ f1.php
@@ -0,0 +1,22 @@
+<?php
+function evalOperation(string $stringOperation){
+    $operation=intval($stringOperation);
+    $operators=["+","-","/","*"];
+    for ($i=0;$i<strlen($stringOperation);$i++){
+        if( $stringOperation[$i]== "+"){
+            return intval($stringOperation[$i-1]) + intval($stringOperation[$i+1]);
+        }
+        elseif ( $stringOperation[$i]== "-"){
+            return intval($stringOperation[$i-1]) - intval($stringOperation[$i+1]);
+        }
+        elseif ( $stringOperation[$i]== "*"){
+            return intval($stringOperation[$i-1]) * intval($stringOperation[$i+1]);
+        }
+        else{
+            return intval($stringOperation[$i-1]) / intval($stringOperation[$i+1]);
+
+        }
+
+    }
+    
+}
